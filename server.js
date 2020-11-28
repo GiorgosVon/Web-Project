b781e16513af0d8011f44b3f7f209b3baa8001ff
VoTheGreat <43538351+VoTheGreat@users.noreply.github.com>
2020-11-29 01:09:30 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'test';
+let db;
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(port, function () {
+    console.log('listening on port 3000')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+});
+
+MongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
+    if (err) return console.log(err);
+
+    db = client.db(dbName);
+    console.log(`Connected MongoDB: ${url}`);
+    console.log(`Database: ${dbName}`);
+})
